6d068ac605962889d42c78477650c1b01f8dfc65
qdl <levinqdl@magicengine.com.cn>
2021-10-21 16:45:43 +0800

    add loading command

--- commands.js
+++ commands.js
@@ -2,3 +2,8 @@ Cypress.on('window:before:load', (win) => {
   // this lets React DevTools "see" components inside application's iframe
   win.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.top.__REACT_DEVTOOLS_GLOBAL_HOOK__
 })
+
+Cypress.Commands.add('loading', (timeout = 4000) => {
+  cy.findByRole('alert').should('exist')
+  cy.findByRole('alert', {timeout}).should('not.exist')
+})
