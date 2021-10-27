60f56ef574e1d6f15c5289bde0970a61fedf5f72
qdl <levinqdl@magicengine.com.cn>
2021-10-27 16:04:59 +0800

    stub window.open to prevent open new tab

--- commands.js
+++ commands.js
@@ -1,9 +1,12 @@
 Cypress.on('window:before:load', (win) => {
   // this lets React DevTools "see" components inside application's iframe
   win.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.top.__REACT_DEVTOOLS_GLOBAL_HOOK__
+  cy.stub(win, 'open').callsFake((url) => {
+    return win.open.wrappedMethod.call(win, url, '_self')
+  })
 })
 
 Cypress.Commands.add('loading', (timeout = 4000) => {
   cy.findByRole('alert').should('exist')
-  cy.findByRole('alert', {timeout}).should('not.exist')
+  cy.findByRole('alert', { timeout }).should('not.exist')
 })
