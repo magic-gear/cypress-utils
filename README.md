09a5ada908e433436d3d77a56fd3f9cbb9125578
levinqdl <levinqdl@gmail.com>
2022-05-05 12:42:45 +0800

    export installReactDevTools

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cypress-utils
+
+## open cypress with chrome react dev tools extension installed
+
+``` javascript
+// cypress/plugins/index.js
+const cypressUtils = require('@magic-gear/cypress-utils')
+
+module.exports = (on, config) => {
+  on('before:browser:launch', (browser, options) => {
+    cypressUtils.installReactDevTools(browser, options)
+    return options
+	})
+}
+```
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const path = require('path')
-module.exports = (on, config) => {
-	if (config && config.watchForFileChanges)
-		on('before:browser:launch', (browser, launchOptions) => {
-
-			// only load React DevTools extension
-			// when opening Chrome in interactive mode
-			if (browser.family === 'chromium') {
-				// we could also restrict the extension
-				// to only load when "browser.isHeaded" is true
-				const extensionFolder = path.resolve(__dirname, 'react-devtools')
-
-				launchOptions.args.push(`--load-extension=${extensionFolder}`)
-
-				return launchOptions
-			}
-		})
+const installReactDevTools = (browser, launchOptions) => {
+	if (browser.family === 'chromium') {
+		if (browser.isHeaded) {
+			const extensionFolder = path.resolve(__dirname, 'react-devtools')
+			launchOptions.args.push(`--load-extension=${extensionFolder}`)
+		}
+	}
+	return launchOptions
 }
+const hookInstallReactDevTools =  (on, config) => {
+	if (config && config.watchForFileChanges) {
+		on('before:browser:launch', installReactDevTools)
+	}
+}
+hookInstallReactDevTools.installReactDevTools = installReactDevTools
+module.exports = hookInstallReactDevTools
